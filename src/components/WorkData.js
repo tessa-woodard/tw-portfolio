a3b42dbaf7a1b3ebd083d521df75e0a556755b3c
Tessa Woodard <tessawoodard@outlook.com>
2021-12-14 12:09:17 -0500

    1st commit
    Signed-off-by: Tessa Woodard <tessawoodard@outlook.com>

---
+++ src/components/WorkData.js
@@ -0,0 +1,61 @@
+export const Work = [
+    {
+        id:1,
+        name:"Urban Roots",
+        description:"Urban Roots is an online plant website with features that include: plants for purchase, login functionality, contact form, FAQ page, inspiration ideas and much more.",
+        tags:["Javascript", "React", "Styled Components", "Gatsby", "GraphQL", "Firebase", "Contentful", "Snipcart", "Formspree.io", "Bootstrap", "CSS", "Material UI", "Netlify"],
+        
+        demo:"https://urban-roots.netlify.app/",
+        github:"https://github.com/tessa-woodard/urban-roots"
+    },
+    {
+        id:2,
+        name:"Impresso Espresso",
+        description:"This project was build to mimic an actual coffee shop website with features that include, customer reviews, menu items, products for purchase, about the company and a contact form.",
+        tags:["Javascript","React", "Styled Components", "Gatsby", "Graph QL", "Contentful, Snipcart", "Formspree.io", "Bootstrap", "CSS", "Netlify"],
+        
+        demo:"https://impresso-expresso.netlify.app/",
+        github:"https://github.com/tessa-woodard/impresso-espresso"
+        
+    },
+    {
+        id:3,
+        name:"Gitbook",
+        description:"Gitbook is a way to search for GitHub users. It will allow you to view the user's languages, most popular repos, stars per language as well as most fork repos.",
+        tags:["Javascript","React","Styled Components", "Context API", "Auth0", "React-Router-Dom", "Fusion Charts", "Github API", "CSS", "Netlify"],
+        
+        demo:"https://tw-gitbook.netlify.app/login",
+        github:"https://github.com/tessa-woodard/gitbook"
+    },
+    // {
+    //     id:4,
+    //     name:"Sidebar Menu",
+    //     description:"A cool looking sidebar menu build using ReactJS and styledComponents.It has really smooth animations.",
+    //     tags:["react","styledComponents","react-router"],
+        
+    //     demo:"https://react-sidebar.vercel.app/",
+    //     github:"https://github.com/codebucks27/react-sidebar"
+       
+    // },{
+    //     id:5,
+    //     name:"Todo App(React + Redux)",
+    //     description:"A todo app build using Redux-toolkit and framer-motion for smooth animations.",
+    //     tags:["react","redux","framer-motion"],
+        
+    //     demo:"http://react-redux-todo-app-git-main-codebucks27.vercel.app/",
+    //     github:"https://github.com/codebucks27/React-Redux-Todo-App"
+        
+    // },
+    // {
+    //     id:6,
+    //     name:"Responsive Navigation Menu",
+    //     description:"A reponsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.",
+    //     tags:["react","react-router","css"],
+        
+    //     demo:"http://react-responsive-navbar-codebucks27.vercel.app/",
+    //     github:"https://github.com/codebucks27/React-responsive-navbar"
+        
+    // },
+    
+  
+] 
